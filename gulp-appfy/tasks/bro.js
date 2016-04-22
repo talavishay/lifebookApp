060b0d6042c3509749859bf92ffa7458903052d0
user <talavishay@gmail.com>
2016-04-22 11:46:38 +0300

    remove gulp from repo

--- gulp-appfy/tasks/bro.js
+++
@@ -1,9 +0,0 @@
-var gulp = require( 'gulp' );
-module.exports = function ( config ) {
-gulp.task('build', () =>
-  gulp.src('index.js')
-    .pipe(bro())
-    .pipe(gulp.dest('dist'))
-)
-
-gulp.watch('*.js', ['build'])
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-/**
- * Gulp tasks to work with browserify, reworkcss and others great tools
- */
-'use strict';
-
-require('./gulp-appfy')(__dirname);
-
