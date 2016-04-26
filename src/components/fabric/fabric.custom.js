2ed0b50d10761ce7e91da8c3a7d88fe166f2e67a
user <talavishay@gmail.com>
2016-04-26 10:29:07 +0300

    remove custom fabric zoom image

--- src/components/fabric/fabric.custom.js
+++ src/components/fabric/fabric.custom.js
@@ -19,132 +19,132 @@ fabric.Canvas.prototype.getAbxxxxxxx = function(object) {
 };
 
 
-fabric.Cropzoomimage = fabric.util.createClass(fabric.Image, {
-	type: 'cropzoomimage',
-	async : true,
-	zoomedXY: false,
-	initialize: function(element, options) {
-		options || (options = {});
-		this.callSuper('initialize', element, options);
-		this.set({
-			orgSrc: element.src,
-			cx: 0, // clip-x
-			cy: 0, // clip-y
-			cw: element.width, // clip-width
-			ch: element.height // clip-height
-		});
-	},
+//~ fabric.Cropzoomimage = fabric.util.createClass(fabric.Image, {
+	//~ type: 'cropzoomimage',
+	//~ async : true,
+	//~ zoomedXY: false,
+	//~ initialize: function(element, options) {
+		//~ options || (options = {});
+		//~ this.callSuper('initialize', element, options);
+		//~ this.set({
+			//~ orgSrc: element.src,
+			//~ cx: 0, // clip-x
+			//~ cy: 0, // clip-y
+			//~ cw: element.width, // clip-width
+			//~ ch: element.height // clip-height
+		//~ });
+	//~ },
 
-	zoomBy: function(x, y, z, callback)
-	{
-		if (x || y) { this.zoomedXY = true; }
-		this.cx += x;
-		this.cy += y;
+	//~ zoomBy: function(x, y, z, callback)
+	//~ {
+		//~ if (x || y) { this.zoomedXY = true; }
+		//~ this.cx += x;
+		//~ this.cy += y;
 
-		if (z) {
-			this.cw -= z;
-			this.ch -= z/(this.width/this.height);
-		}
+		//~ if (z) {
+			//~ this.cw -= z;
+			//~ this.ch -= z/(this.width/this.height);
+		//~ }
 
-		if (z && !this.zoomedXY) {
-			// Zoom to center of image initially
-			this.cx = this.width / 2 - (this.cw / 2);
-			this.cy = this.height / 2 - (this.ch / 2);
-		}
+		//~ if (z && !this.zoomedXY) {
+			//~ // Zoom to center of image initially
+			//~ this.cx = this.width / 2 - (this.cw / 2);
+			//~ this.cy = this.height / 2 - (this.ch / 2);
+		//~ }
 
-		if (this.cw > this.width) { this.cw = this.width; }
-		if (this.ch > this.height) { this.ch = this.height; }
-		if (this.cw < 1) { this.cw = 1; }
-		if (this.ch < 1) { this.ch = 1; }
-		if (this.cx < 0) { this.cx = 0; }
-		if (this.cy < 0) { this.cy = 0; }
-		if (this.cx > this.width - this.cw) { this.cx = this.width - this.cw; }
-		if (this.cy > this.height - this.ch) { this.cy = this.height - this.ch; }
+		//~ if (this.cw > this.width) { this.cw = this.width; }
+		//~ if (this.ch > this.height) { this.ch = this.height; }
+		//~ if (this.cw < 1) { this.cw = 1; }
+		//~ if (this.ch < 1) { this.ch = 1; }
+		//~ if (this.cx < 0) { this.cx = 0; }
+		//~ if (this.cy < 0) { this.cy = 0; }
+		//~ if (this.cx > this.width - this.cw) { this.cx = this.width - this.cw; }
+		//~ if (this.cy > this.height - this.ch) { this.cy = this.height - this.ch; }
 
-		this.rerender(callback);
-	},
+		//~ this.rerender(callback);
+	//~ },
 
-	rerender: function(callback)
-	{
-		var img = new Image(), obj = this;
-		img.onload = function() {
-			var canvas = fabric.util.createCanvasElement();
-			canvas.width = obj.width;
-			canvas.height = obj.height;
-			canvas.getContext('2d').drawImage(this, obj.cx, obj.cy, obj.cw, obj.ch, 0,0, obj.width, obj.height);
+	//~ rerender: function(callback)
+	//~ {
+		//~ var img = new Image(), obj = this;
+		//~ img.onload = function() {
+			//~ var canvas = fabric.util.createCanvasElement();
+			//~ canvas.width = obj.width;
+			//~ canvas.height = obj.height;
+			//~ canvas.getContext('2d').drawImage(this, obj.cx, obj.cy, obj.cw, obj.ch, 0,0, obj.width, obj.height);
 
-			img.onload = function() {
-				obj.setElement(this);
-				obj.applyFilters(window.App.canvas.renderAll.bind(window.App.canvas));
-				obj.set({
-					left: obj.left,
-					top: obj.top,
-					angle: obj.angle
-				});
-				obj.setCoords();
-				if (callback) { callback(obj); }
-			};
-			img.src = canvas.toDataURL('image/png');
-		};
-		img.src = this.orgSrc;
-	},
+			//~ img.onload = function() {
+				//~ obj.setElement(this);
+				//~ obj.applyFilters(window.App.canvas.renderAll.bind(window.App.canvas));
+				//~ obj.set({
+					//~ left: obj.left,
+					//~ top: obj.top,
+					//~ angle: obj.angle
+				//~ });
+				//~ obj.setCoords();
+				//~ if (callback) { callback(obj); }
+			//~ };
+			//~ img.src = canvas.toDataURL('image/png');
+		//~ };
+		//~ img.src = this.orgSrc;
+	//~ },
 
-	toObject: function()
-	{
-		return fabric.util.object.extend(this.callSuper('toObject'), {
-			orgSrc: this.orgSrc,
-			cx: this.cx,
-			cy: this.cy,
-			cw: this.cw,
-			ch: this.ch
-		});
-	}
-});
-fabric.Cropzoomimage.fromObject = function(object, callback) {
-	fabric.util.loadImage(object.src, function(img) {
-		fabric.Image.prototype._initFilters.call(object, object, function(filters) {
-			object.filters = filters || [];
-			var instance = new fabric.Cropzoomimage(img, object);
-			if (callback) { callback(instance); }
-		});
-	}, null, object.crossOrigin);
-};
-fabric.Image.filters.Tint.prototype.applyTo = function (canvasEl) {
-    var context = canvasEl.getContext('2d'),
-        imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
-        data = imageData.data,
-        iLen = data.length,
-        i,
-        tintR, tintG, tintB,
-        r, g, b, alpha1,
-        source;
+	//~ toObject: function()
+	//~ {
+		//~ return fabric.util.object.extend(this.callSuper('toObject'), {
+			//~ orgSrc: this.orgSrc,
+			//~ cx: this.cx,
+			//~ cy: this.cy,
+			//~ cw: this.cw,
+			//~ ch: this.ch
+		//~ });
+	//~ }
+//~ });
+//~ fabric.Cropzoomimage.fromObject = function(object, callback) {
+	//~ fabric.util.loadImage(object.src, function(img) {
+		//~ fabric.Image.prototype._initFilters.call(object, object, function(filters) {
+			//~ object.filters = filters || [];
+			//~ var instance = new fabric.Cropzoomimage(img, object);
+			//~ if (callback) { callback(instance); }
+		//~ });
+	//~ }, null, object.crossOrigin);
+//~ };
+//~ fabric.Image.filters.Tint.prototype.applyTo = function (canvasEl) {
+    //~ var context = canvasEl.getContext('2d'),
+        //~ imageData = context.getImageData(0, 0, canvasEl.width, canvasEl.height),
+        //~ data = imageData.data,
+        //~ iLen = data.length,
+        //~ i,
+        //~ tintR, tintG, tintB,
+        //~ r, g, b, alpha1,
+        //~ source;
 
-    source = new fabric.Color(this.color).getSource();
+    //~ source = new fabric.Color(this.color).getSource();
 
-    tintR = source[0] * this.opacity;
-    tintG = source[1] * this.opacity;
-    tintB = source[2] * this.opacity;
+    //~ tintR = source[0] * this.opacity;
+    //~ tintG = source[1] * this.opacity;
+    //~ tintB = source[2] * this.opacity;
 
-    alpha1 = 1 - this.opacity;
+    //~ alpha1 = 1 - this.opacity;
 
-    for (i = 0; i < iLen; i += 4) {
-        r = data[i];
-        g = data[i + 1];
-        b = data[i + 2];
+    //~ for (i = 0; i < iLen; i += 4) {
+        //~ r = data[i];
+        //~ g = data[i + 1];
+        //~ b = data[i + 2];
 
-        // fabric's actual computation
-        data[i] = tintR + r * alpha1;
-        data[i + 1] = tintG + g * alpha1;
-        data[i + 2] = tintB + b * alpha1;
+        //~ // fabric's actual computation
+        //~ data[i] = tintR + r * alpha1;
+        //~ data[i + 1] = tintG + g * alpha1;
+        //~ data[i + 2] = tintB + b * alpha1;
 
-        // Luc's computation
-        data[i] = (tintR * r) / 255;
-        data[i + 1] = (tintG * g) / 255;
-        data[i + 2] = (tintB * b) / 255;
-    }
+        //~ // Luc's computation
+        //~ data[i] = (tintR * r) / 255;
+        //~ data[i + 1] = (tintG * g) / 255;
+        //~ data[i + 2] = (tintB * b) / 255;
+    //~ }
 
-    context.putImageData(imageData, 0, 0);
-};
+    //~ context.putImageData(imageData, 0, 0);
+//~ };
 
 
 // fabric_canvas)view ## START
