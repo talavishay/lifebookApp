1ed5610ba8e9da1ecde2b9ad2785ec762fdaffc5
user <user@debian>
2016-05-23 15:42:58 +0300

    add new component => chapterResources:  load all chapter data in one request

---
+++ src/components/models/book/chapters/chapterResources.js
@@ -0,0 +1,15 @@
+var chapterResources = {
+	idAttribute	: "id",
+	urlRoot		: '/lifebook/app/resources/chapter',
+	initialize	: function(){
+		App.Backbone.Model.prototype.initialize.apply(this, arguments);
+		this.on({
+			"sync"	: this._initChapter
+		}, this);
+	},
+	_initChapter	: function(data){
+		App.bookChannel.trigger("init:chapter", data);
+	}
+};
+module.exports = Backbone.Model.extend(chapterResources);
+
